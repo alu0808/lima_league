525d4a378efe76953dca53f1762f8d06e9749dfc
AXL VEJARANO <axl.vejarano@qds.com.pe>
2025-09-12 02:25:50 -0500

    deploy boards in matches

--- matches/api/urls.py
+++ matches/api/urls.py
@@ -1,10 +1,11 @@
 # matches/urls.py
 from django.urls import path
 
-from .views import UpcomingMatchesView, MatchDetailView, JoinMatchView, LeaveMatchView
+from .views import UpcomingMatchesView, MatchDetailView, JoinMatchView, LeaveMatchView, MatchesBoardView
 
 urlpatterns = [
     path("matches/upcoming", UpcomingMatchesView.as_view(), name="matches-upcoming"),
+    path("matches/board", MatchesBoardView.as_view(), name="matches-board"),
     path("matches/<uuid:match_identifier>", MatchDetailView.as_view(), name="matches-detail"),
     path("matches/<uuid:match_identifier>/join", JoinMatchView.as_view(), name="matches-join"),
     path("matches/<uuid:match_identifier>/leave", LeaveMatchView.as_view(), name="matches-leave"),
